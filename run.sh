105d92397e1062230917e7d6fe10b50b3b7a32ae
stghoul <st.ghoul@gmail.com>
2021-08-23 22:36:16 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@
 sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 service sshd restart
 
-#creating /etc/rc.local
-curl https://raw.githubusercontent.com/stghoul/debian_install_some_goods/main/rc-local.service --output /etc/systemd/system/rc-local.service
-curl https://raw.githubusercontent.com/stghoul/debian_install_some_goods/main/rc.local --output /etc/rc.local
-chmod +x /etc/rc.local
-systemctl enable rc-local
-systemctl start rc-local
-
 #installing some useful stuff
 apt update
 apt install -y wget sudo
+
+#creating /etc/rc.local
+wget https://raw.githubusercontent.com/stghoul/debian_install_some_goods/main/rc-local.service --output=/etc/systemd/system/rc-local.service
+wget https://raw.githubusercontent.com/stghoul/debian_install_some_goods/main/rc.local --output=/etc/rc.local
+chmod +x /etc/rc.local
+systemctl enable rc-local
+systemctl start rc-local
